bed99f5fc0d5a1dbd9c4d9f200c1e51a4e637e25
ahermez <131738383+ahermez@users.noreply.github.com>
2023-10-16 11:24:23 -0700

    save button working

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,15 +1,22 @@
 // see SignupForm.js for comments
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import {LOGIN_USER} from "../utils/mutations"
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-
+  const [login, {err}] = useMutation(LOGIN_USER)
+  useEffect(() => {
+    if (err) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [err]);
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -26,15 +33,12 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
+    
+      console.log(data);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
